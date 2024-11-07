811ed527309a0cc90f39cc25c69eb92c82d94c4c
Michael-Andryeer <andryeerm@gmail.com>
2024-11-07 01:13:40 -0300

    Reconfigurei o Knex pra usar sqlite, estava tendo dificuldades pra usar o postgres

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,8 @@
-import { knex as setupKnex } from "knex";
+import {knex as setupKnex} from 'knex'
 
 export const knex = setupKnex({
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-        host: 'localhost',  // endereço do seu banco de dados
-        port: 5432, // porta padrão do PostgreSQL
-        user: 'postgres',// seu usuário do PostgreSQL
-        password: '123',// sua senha
-        database: 'nodeapi'// nome do seu banco de dados
+        filename: './tmp/app.db',
     }
-});
+})
