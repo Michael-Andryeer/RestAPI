e132d1de775fd16f99ff0b31a957336569d99d7f
Michael-Andryeer <andryeerm@gmail.com>
2024-11-08 01:09:40 -0300

    Normalizando a porta

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import { version } from 'os';
 
 const app = express()
-const port = 3000
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port',port)
 
 const server = http.createServer(app)
@@ -12,7 +12,7 @@ const router = express.Router()
 
 const route = router.get('/', (request,response,next) => {
     response.status(200).send({
-        title: "Node store",
+        title: "Node store API",
         version: "0.0.1"
     })
 })
@@ -20,3 +20,17 @@ app.use('/',route)
 
 server.listen(port)
 console.log('API rodando na porta ' + port)
+
+function normalizePort(value) {
+    const port = parseInt(value,10)
+
+    if(isNaN(port)){ // se não for um número
+        return value
+    }
+
+    if(port >= 0) { //Retorna a porta
+        return port
+    }
+
+    return false
+}
